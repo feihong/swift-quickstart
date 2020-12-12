7efafd0ea853245d37ee10f4f6a1008c311ce067
Feihong Hsu <feihong.hsu@gmail.com>
2020-12-11 19:25:38 -0600

    Add basic itunes script

---
+++ itunes_quickstart.swift
@@ -0,0 +1,24 @@
+import Foundation
+import ScriptingBridge
+
+@objc protocol iTunesTrack {
+    @objc optional var name: String {get}
+    @objc optional var album: String {get}
+}
+
+@objc protocol iTunesApplication {
+    @objc optional var soundVolume: Int {get}
+    @objc optional var currentTrack: iTunesTrack? {get}
+}
+
+extension SBApplication : iTunesApplication {}
+
+if let app: iTunesApplication = SBApplication(bundleIdentifier: "com.apple.Music") {
+  // Because these are all optional properties (to avoid providing an implementation), we have
+  // to use '!' to indicate we know the implementation exists.
+  let track: iTunesTrack? = app.currentTrack!
+  let album = track?.album!
+  let trackName = track?.name!
+
+  print("Current track: \(String(describing: trackName)) - \(String(describing: album))")
+}
