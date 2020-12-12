20211c4ecf2bc2b84eb0ac600aa821222b133074
Feihong Hsu <feihong.hsu@gmail.com>
2020-12-11 19:28:09 -0600

    Print lyrics

--- itunes_quickstart.swift
+++ itunes_quickstart.swift
@@ -2,13 +2,14 @@ import Foundation
 import ScriptingBridge
 
 @objc protocol iTunesTrack {
-    @objc optional var name: String {get}
-    @objc optional var album: String {get}
+  @objc optional var name: String {get}
+  @objc optional var album: String {get}
+  @objc optional var lyrics: String {get}
 }
 
 @objc protocol iTunesApplication {
-    @objc optional var soundVolume: Int {get}
-    @objc optional var currentTrack: iTunesTrack? {get}
+  @objc optional var soundVolume: Int {get}
+  @objc optional var currentTrack: iTunesTrack? {get}
 }
 
 extension SBApplication : iTunesApplication {}
@@ -19,6 +20,8 @@ if let app: iTunesApplication = SBApplication(bundleIdentifier: "com.apple.Music
   let track: iTunesTrack? = app.currentTrack!
   let album = track?.album!
   let trackName = track?.name!
+  let lyrics = track?.lyrics!
 
   print("Current track: \(String(describing: trackName)) - \(String(describing: album))")
+  print("Lyrics: \(String(describing: lyrics))")
 }
