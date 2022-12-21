e7139f45f6038c7d8c43941952ca5698b48ebefb
Feihong Hsu <feihong.hsu@gmail.com>
2022-12-21 10:02:21 -0600

    Set rating of current track to 5 stars

--- music_scriptingbridge_quickstart.swift
+++ music_scriptingbridge_quickstart.swift
@@ -2,20 +2,23 @@ import Foundation
 import ScriptingBridge
 
 @objc protocol MusicTrack {
-  @objc optional var name: String {get}
-  @objc optional var album: String {get}
-  @objc optional var artist: String {get}
-  @objc optional var genre: String {get}
-  @objc optional var lyrics: String {get}
-  @objc optional var comment: String {get}
-  @objc optional var rating: Int {get}
-  @objc optional var dateAdded: Date {get}
-  @objc optional var modificationDate: Date {get}
+  @objc optional var name: String { get }
+  @objc optional var album: String { get }
+  @objc optional var artist: String { get }
+  @objc optional var genre: String { get }
+  @objc optional var lyrics: String { get }
+  @objc optional var comment: String { get }
+  @objc optional var rating: Int { get }
+  @objc optional var dateAdded: Date { get }
+  @objc optional var modificationDate: Date { get }
+
+  @objc optional var databaseID: Int { get }
+  @objc optional func setRating(_ rating: Int)
 }
 
 @objc protocol MusicApplication {
-  @objc optional var soundVolume: Int {get}
-  @objc optional var currentTrack: MusicTrack {get}
+  @objc optional var soundVolume: Int { get }
+  @objc optional var currentTrack: MusicTrack { get }
 }
 
 extension SBApplication : MusicApplication {}
@@ -30,10 +33,11 @@ guard let track: MusicTrack = app.currentTrack else {
   exit(1)
 }
 
-let lyrics = track.lyrics!.replacingOccurrences(of: "\r", with: "\n", options: .literal, range: nil)
-
-print("Current track: \(track.name!) by \(track.artist!)")
+print("Current track: \(track.name!) by \(track.artist!), database ID: \(track.databaseID!)")
 print("Rating: \(track.rating! / 20) stars")
 print("Modified: \(track.modificationDate!), Added: \(track.dateAdded!)")
-print("Lyrics:\n\(lyrics)")
+let lyrics = track.lyrics!.replacingOccurrences(of: "\r", with: "\n", options: .literal, range: nil)
+// print("Lyrics:\n\(lyrics)")
 
+print("Set rating to 5 stars")
+track.setRating!(100)
